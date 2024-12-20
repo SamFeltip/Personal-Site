42d7d751eefb4ede80ff8f571091f708b8c814ba
Sam Felton <sf.samfelton@icloud.com>
2024-12-20 23:13:41 +0000

    added more spacing for articles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,15 @@ module.exports = {
         playfair: ['"Playfair Display"', "serif"],
         roboto: ['"Roboto"', "serif"],
       },
+      keyframes: {
+        show: {
+          "0%": { opacity: 0, transform: "translateY(20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        show: "show 0.5s ease-out forwards",
+      },
     },
   },
   plugins: [],
