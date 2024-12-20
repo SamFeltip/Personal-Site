aff35a635ea39151255c9f7e9d42a150151e686c
Sam Felton <sf.samfelton@icloud.com>
2024-12-20 22:56:47 +0000

    added more components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', "serif"],
+        roboto: ['"Roboto"', "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
